0ce0f7cad5e9c006519caf0688cf126d0e2a47cc
Harsh Vardhan <devharsh2k4@gmail.com>
2024-09-25 21:01:55 +0530

    added authentication

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -16,7 +16,7 @@ export const Header = () => {
     <header className="h-20 w-full border-b-2 border-slate-200 px-4 ">
       <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
         <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
-          <Image src="/mascot.png" alt="logo" height={40} width={40} />
+          <Image src="/mascot.svg" alt="logo" height={40} width={40} />
           <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
             LangLearn
           </h1>
@@ -27,7 +27,7 @@ export const Header = () => {
         <ClerkLoaded>
           <SignedIn>
             <UserButton
-            after
+            
             />
           </SignedIn>
           <SignedOut>
