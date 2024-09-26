83d64278f3307a7663f9d6b176f3d58bebb89627
Harsh Vardhan <devharsh2k4@gmail.com>
2024-09-27 04:03:59 +0530

    added seed script

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -4,6 +4,7 @@ import { Header } from "./header";
 import { getUserProgress } from "@/db/queries";
 import { UserProgress } from "@/components/user-progress";
 import { redirect } from "next/navigation";
+import { use } from "react";
 
 
 const LearnPage = async () => {
@@ -18,14 +19,14 @@ const LearnPage = async () => {
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
-          activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
-          hearts={5}
-          points={100}
-          hasActiveSubscription={true}
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+          hasActiveSubscription={false}
         />
       </StickyWrapper>
       <FeedWrapper>
-        <Header title="spanish" />
+        <Header title={userProgress.activeCourse.title}/>
     
       </FeedWrapper>
     </div>
