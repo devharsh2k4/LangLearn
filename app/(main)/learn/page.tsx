1844bc9db3c5d4a86f0e111374903e7bafad894e
Harsh Vardhan <devharsh2k4@gmail.com>
2024-09-27 11:28:14 +0530

    added units

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,19 +1,22 @@
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { Header } from "./header";
-import { getUserProgress } from "@/db/queries";
+import { getUnits, getUserProgress } from "@/db/queries";
 import { UserProgress } from "@/components/user-progress";
 import { redirect } from "next/navigation";
 import { use } from "react";
 
-
 const LearnPage = async () => {
   const userProgerssData = getUserProgress();
+  const unitsData = getUnits();
 
-  const [userProgress] = await Promise.all([userProgerssData]);
+  const [userProgress, units] = await Promise.all([
+    userProgerssData,
+    unitsData,
+  ]);
 
-  if(!userProgress || !userProgress.activeCourse){
-  redirect("/courses");
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
   }
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
@@ -26,8 +29,12 @@ const LearnPage = async () => {
         />
       </StickyWrapper>
       <FeedWrapper>
-        <Header title={userProgress.activeCourse.title}/>
-    
+        <Header title={userProgress.activeCourse.title} />
+        {units.map((unit) => (
+          <div key={unit.id} className="mb-10">
+            {JSON.stringify(unit)}
+          </div>
+        ))}
       </FeedWrapper>
     </div>
   );
